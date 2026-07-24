aa5240fadaba692103397f37270a6cfc28a577f3
Arpita Taware <arpitataware44@gmail.com>
2026-07-24 22:50:23 +0530

    Implement coin arrangement solution with binary search

---
+++ Java/0441-Arranging-Coins/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    int sqrt(long n) {
+        if (n == 0)
+            return 0;
+        long lo = 0, hi = n;
+        while (lo <= hi) {
+            long mid = lo + (hi - lo) / 2;
+            if (mid == n / mid)
+                return (int) mid;
+            else if (mid > n / mid)
+                hi = mid - 1;
+            else
+                lo = mid + 1;
+        }
+        return (int) hi;
+
+    }
+
+    public int arrangeCoins(int n) {
+        long m= (long) n;
+        return (sqrt(8 * m + 1)-1) / 2;
+    }
+}
